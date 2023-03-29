ef559f6ca4559c1e7542a7f4d9288107064fef33
Evgeniy-Pavlov <pavlovev@it2g.ru>
2023-03-29 22:06:29 +0300

    поправил работу стилей

--- not_confluence/not_confluence/settings.py
+++ not_confluence/not_confluence/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'not_confluence.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
