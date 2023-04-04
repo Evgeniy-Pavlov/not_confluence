fe84b2e6d75d90e60d429dd2ac45ea43d7a52ce8
Evgeniy-Pavlov <pavlovev@it2g.ru>
2023-04-04 20:32:41 +0300

    начало работы с тестами

--- not_confluence/mainapp/tests.py
+++ not_confluence/mainapp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import User, Project, Article
 
 # Create your tests here.
+
+class TestUser(TestCase):
+    
+    def setUp(self) -> None:
+        user = User.objects.create(login='semenovse', last_name='Семенов', first_name='Семен', patronymic='Евгеньевич')
+
+
+    def test_init(self):
+        self.assertEqual(self.login, 'semenovse')
